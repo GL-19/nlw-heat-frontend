c521a6184014399043fe6db858eb070e9a7d1f58
GL-19 <loiolagl19@gmail.com>
2021-10-21 09:43:17 -0300

    Estilização da MessageList finalizada

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,3 +1,42 @@
+import styles from "./styles.module.scss";
+
+import logoImg from "../../assets/logo.svg";
+
 export function MessageList() {
-	return <h1>MessageList</h1>;
+	return (
+		<div className={styles.messageListWrapper}>
+			<img src={logoImg} alt="DoWhile 2021" />
+			<ul className={styles.messageList}>
+				<li className={styles.message}>
+					<p className={styles.messageContent}> Não vejo a hora de começar a testar</p>
+					<div className={styles.messageUser}>
+						<div className={styles.userImage}>
+							<img src="https://github.com/GL-19.png" alt="Glauber Loiola" />
+						</div>
+						<span>Glauber Loiola</span>
+					</div>
+				</li>
+
+				<li className={styles.message}>
+					<p className={styles.messageContent}> Não vejo a hora de começar a testar</p>
+					<div className={styles.messageUser}>
+						<div className={styles.userImage}>
+							<img src="https://github.com/GL-19.png" alt="Glauber Loiola" />
+						</div>
+						<span>Glauber Loiola</span>
+					</div>
+				</li>
+
+				<li className={styles.message}>
+					<p className={styles.messageContent}> Não vejo a hora de começar a testar</p>
+					<div className={styles.messageUser}>
+						<div className={styles.userImage}>
+							<img src="https://github.com/GL-19.png" alt="Glauber Loiola" />
+						</div>
+						<span>Glauber Loiola</span>
+					</div>
+				</li>
+			</ul>
+		</div>
+	);
 }
