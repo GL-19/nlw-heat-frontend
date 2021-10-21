ec7d1c02b2b52e442068ef0a8e716bacf564ef58
GL-19 <loiolagl19@gmail.com>
2021-10-21 13:37:14 -0300

    Aplicação finalizada

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -14,9 +14,32 @@ type Message = {
 	};
 };
 
+const messagesQueue: Message[] = [];
+
 const socket = io("http://localhost:4000");
+
+socket.on("new_message", (newMessage: Message) => {
+	console.log(newMessage);
+	messagesQueue.push(newMessage);
+});
+
 export function MessageList() {
 	const [messages, setMessages] = useState<Message[]>([]);
+
+	useEffect(() => {
+		setInterval(() => {
+			if (messagesQueue.length > 0) {
+				//prettier-ignore
+				setMessages(prevState => [
+          messagesQueue[0], 
+          prevState[0], 
+          prevState[1]
+        ].filter(Boolean));
+
+				messagesQueue.shift();
+			}
+		}, 3000);
+	}, []);
 
 	useEffect(() => {
 		api.get<Message[]>("messages/last3").then((response) => {
